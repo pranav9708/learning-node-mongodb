6f5fbf3c27bd1551657d8aed1789fe01e184e3cf
Pranav V <pranav9708@gmail.com>
2022-11-30 11:42:39 +0530

    create partials and layouts

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 //by default website run on port 80
 const port =8000;
+const expressLayout= require('express-ejs-layouts');
+
+app.use(expressLayout);
 
 app.use('/',require('./routes'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 
 app.listen(port, function(err){

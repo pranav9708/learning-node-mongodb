799011217f1cf3e65921ab1b084fb31e5a7046fa
Pranav V <pranav9708@gmail.com>
2022-12-26 18:47:25 +0530

    complete user Sign up

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,9 @@ route.get('/profile',usersController.profile);
 route.get('/sign-in',usersController.signIn);
 
 route.get('/sign-up',usersController.signUp);
+
+route.post('/create', usersController.create);
+
+
 module.exports = route;
+

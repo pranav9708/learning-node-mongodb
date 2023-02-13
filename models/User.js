a5bd90941f66258e11164c3780fea80885977b10
Pranav V <pranav9708@gmail.com>
2023-02-13 12:24:35 +0530

    configuring multer avater

--- models/User.js
+++ models/User.js
@@ -25,6 +25,17 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 });
 
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..',AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+
+  
 
 const User = mongoose.model('User', userSchema);
 

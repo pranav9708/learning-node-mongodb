a081c08aceb93906fc480c367ba5ee6fc96bf7b5
Pranav V <pranav9708@gmail.com>
2023-02-13 13:29:33 +0530

    saved user avatar

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -84,12 +84,37 @@ module.exports.destroySession = function(req, res){
     });
 }
 
-module.exports.update=function(req, res){
+module.exports.update= async function(req, res){
+    // if(req.user.id==req.params.id){
+    //     User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+    //         req.flash('success','Updated!');
+    //         return res.redirect('back');
+    //     })
+    // }else{
+    //     req.flash('error','unauthorized');
+    //     return res.status(401).send('Unauthorized')
+    // }
     if(req.user.id==req.params.id){
-        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
-            req.flash('success','Updated!');
+        try{
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req,res,function(err){
+                if(err){
+                    console.log('****multerError****',err)
+                }
+                user.name=req.body.name;
+                user.email=req.body.email;
+
+                if(req.file){
+                    //this is saving the path of thee uploaded ile into the avatar field in the user
+                    user.avatar= User.avatarPath + '/' +req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            })
+        }catch(err){
+            req.flash('error', err);
             return res.redirect('back');
-        })
+        }
     }else{
         req.flash('error','unauthorized');
         return res.status(401).send('Unauthorized')
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const db = require('./config/mongoose');
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
+const passportLocal = require('./config/passport-local-statergy');
 const MongoStore = require('connect-mongo');
 const sassMiddleware =require('node-sass-middleware');
 const flash = require('connect-flash');

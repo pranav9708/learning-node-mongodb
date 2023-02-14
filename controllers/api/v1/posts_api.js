248a185c8ede89311fded77ab5be977a1479d64a
Pranav V <pranav9708@gmail.com>
2023-02-14 18:04:38 +0530

    getting posts list using api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,23 @@
-module.exports.index = function(req,res){
+const Post=require('../../../models/Post');
+
+module.exports.index = async function(req,res){
+
+    let posts= await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate:{
+                    path:'user'
+                },
+                options:{
+                    sort:{
+                        createdAt: -1
+                    }
+                }
+            });
+
     return res.status(200).json({
         message: "Lists of posts",
-        posts: []
+        posts: posts
     })
 }

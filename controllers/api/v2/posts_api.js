1c0acdd838223e9b82e4bc88c2560a29e9489cd9
Pranav V <pranav9708@gmail.com>
2023-02-14 15:45:28 +0530

    adding 2nd api version

--- controllers/api/v2/posts_api.js
+++ controllers/api/v2/posts_api.js
@@ -1,6 +1,6 @@
 module.exports.index = function(req,res){
     return res.status(200).json({
-        message: "Lists of posts",
+        message: "Lists of posts 2nd version",
         posts: []
     })
 }

8a5549e8f1746274f9867937cba13255dca0e38c
Pranav V <pranav9708@gmail.com>
2023-02-15 09:44:26 +0530

    deleting using post api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,4 +1,5 @@
 const Post=require('../../../models/Post');
+const Comment=require('../../../models/comment');
 
 module.exports.index = async function(req,res){
 
@@ -20,4 +21,23 @@ module.exports.index = async function(req,res){
         message: "Lists of posts",
         posts: posts
     })
+}
+
+module.exports.destroy = async function (req, res) {
+    try {
+        let post = await Post.findById(req.params.id);
+
+            post.remove();
+
+            await Comment.deleteMany({ post: req.params.id });
+            
+            return res.json(200,{
+                message: 'POst and associated comments deleted successfully'
+            })
+    } catch (err) {
+        console.log('*****',err);
+        return res.json(500,{
+            message:"Internal Server Error!",
+        });
+    }
 }
--- models/Post.js
+++ models/Post.js
@@ -20,6 +20,6 @@ const postSchema = new mongoose.Schema({
 },{
     timestamps: true
 });
-
-const Post = mongoose.model('Post', postSchema);
-module.exports = Post;
+module.exports = mongoose.models['Post'] || mongoose.model('Post', postSchema)
+// const Post = mongoose.model('Post', postSchema);
+// module.exports = Post;

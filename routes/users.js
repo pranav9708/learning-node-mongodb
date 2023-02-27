db944b0fa7727f338240d31c9d5234dbf854b018
Pranav V <pranav9708@gmail.com>
2023-02-27 12:10:23 +0530

    using google-strategy

--- routes/users.js
+++ routes/users.js
@@ -22,4 +22,8 @@ router.post('/create-session', passport.authenticate(
 
 router.get('/sign-out', usersController.destroySession);
 
+//scope is the information we are looking to fetch
+router.get('/auth/google',passport.authenticate('google',{scope:['profile','email']}));
+router.get('/auth/google/callback',passport.authenticate('google',{failureRedirect: '/users/sign-in'}),usersController.createSession)
+
 module.exports = router;

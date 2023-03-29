9d178f09eda7f8df27692285969259780f8aa34b
Pranav V <pranav9708@gmail.com>
2023-03-29 16:29:14 +0530

    socket connection established

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,5 +1,20 @@
 //observer in socket system
 
 module.exports.chatSockets=function(socketServer){
+    //io will be handling the connections
+    let io=require('socket.io')(socketServer,{
+        cors:{
+            origin: '*'
+        }
+    });
+    
+    io.sockets.on('connection', function(socket){
+        console.log('new connection received',socket.id);
+
+        socket.on('disconnect', function(){
+            console.log('socket disconnected')
+        })
+    })
+
 
 }

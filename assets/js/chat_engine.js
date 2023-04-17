a97a714943d79e5ee7a40d449f2c38812421355a
Pranav V <pranav9708@gmail.com>
2023-04-17 20:21:29 +0530

    sending message via socket

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -13,8 +13,36 @@ class ChatEngine{
 
 
     connectionHandler(){
+
+        let self=this;
+
         this.socket.on('connect',function(){
             console.log('connection established using sockets..!');
+            
+            //send request to join room
+            self.socket.emit('join_room',{
+                user_email:self.userEmail,
+                chatRoom:'codeial'
+            });
+
+            self.socket.on('user_joined',function(data){
+                console.log('a new user joined',data);
+            })
+
+        });
+
+        //send a message on clicking the send message button
+        $('#send-message').click(function(){
+            let msg=$('#chat-message-input').val();
+
+            if(msg!=''){
+                self.socket.emit('sendMessage',{
+                    message:msg,
+                    user_email:self.userEmail,
+                    chatRoom:'codeial'
+                });
+            }
         })
+
     }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -12,8 +12,24 @@ module.exports.chatSockets=function(socketServer){
         console.log('new connection received',socket.id);
 
         socket.on('disconnect', function(){
-            console.log('socket disconnected')
+            console.log('socket disconnected');
         })
+
+        //receiving request
+        socket.on('join_room',function(data){
+            console.log('joining request received',data);
+
+            //if chatroom already exits user will enter tat chatroom
+            socket.join(data.chatRoom);
+
+            //emit user has entered to everyone else in chatRoom
+            io.in(data.chatRoom).emit('user_joined',data);
+        })
+
+        socket.on('send_message', function(data) {
+            io.in(data.chatRoom).emit('receive_message',data);
+        });
+
     })
 
 
